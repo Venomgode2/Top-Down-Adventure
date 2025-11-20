1b3c925e3c78511bdf43e8ac3f3b668aff80aae4
Veno <venomgode2@gmail.com>
2025-11-20 18:46:36 -0500

    Movement!

---
+++ Totality-(TDA)/Assets/Scripts/Player-Movement.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+
+    // FEATURE LIST
+    // 1. Use arrow keys for movement
+    // 2. Need a variable for speed
+    // 3. We need to store the direction of our character
+    // 4. need a variable for running
+    // 5. Special movements example: rolling or dashing
+    // 6. Mantling
+    // 7. If we can climb or not
+
+    private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = moveInput * moveSpeed; 
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+
+        animator.SetBool("IsWalking", true);
+        if (context.canceled)
+        {
+            animator.SetBool("IsWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+        moveInput = context.ReadValue<Vector2>();
+
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+
+
+    }
+}
